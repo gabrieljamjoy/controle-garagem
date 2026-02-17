42029cb4df0b914827de60b16ac6eba704b912d0
gabrieljamjoy <gabriel.jamjoy@gmail.com>
2026-02-17 10:40:00 -0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA6L5UCuz2zlNkIg-UrViq7KHoiBpWJv7E",
+  authDomain: "controle-de-garagem.firebaseapp.com",
+  projectId: "controle-de-garagem",
+  storageBucket: "controle-de-garagem.firebasestorage.app",
+  messagingSenderId: "1061306843396",
+  appId: "1:1061306843396:web:0a4c1c53e3e480bf0ed051"
+};
